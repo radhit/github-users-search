a5ae9c54d97a14198efe1e7905e692c999759234
Riska Adhita <riska.adhita079@gmail.com>
2020-10-14 12:25:57 +0700

    feat: add smart refresh

--- app/src/main/java/com/example/riskaadhita/adapter/UsersAdapter.kt
+++ app/src/main/java/com/example/riskaadhita/adapter/UsersAdapter.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat.getColor
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
--- app/src/main/java/com/example/riskaadhita/view/MainActivity.kt
+++ app/src/main/java/com/example/riskaadhita/view/MainActivity.kt
@@ -11,6 +11,7 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.LinearLayout
 import android.widget.TextView.OnEditorActionListener
 import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
 import com.example.riskaadhita.R
 import com.example.riskaadhita.adapter.UsersAdapter
 import com.example.riskaadhita.api.DataResponse
@@ -18,6 +19,8 @@ import com.example.riskaadhita.base.BaseActivity
 import com.example.riskaadhita.data.Item
 import com.example.riskaadhita.helper.EndlessScrollListener
 import com.example.riskaadhita.helper.RecyclerViewClickListener
+import com.scwang.smartrefresh.header.WaterDropHeader
+import com.scwang.smartrefresh.layout.listener.OnRefreshListener
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -48,7 +51,6 @@ class MainActivity : BaseActivity() {
         et_search.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(p0: Editable?) {
                 if (!p0.toString().equals("")){
-                    println("masuk after text")
                     lastEdit = System.currentTimeMillis()
                     handler.postDelayed(finishChecker, delay)
                 }
@@ -59,7 +61,6 @@ class MainActivity : BaseActivity() {
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 if (!p0.toString().equals("")) {
-                    println("masuk on text")
                     handler.removeCallbacks(finishChecker)
                 }
             }
@@ -76,46 +77,63 @@ class MainActivity : BaseActivity() {
             }
             false
         })
+        refresh_layout.setRefreshHeader(WaterDropHeader(this))
+        refresh_layout.isEnabled = false
+        refresh_layout.setOnRefreshListener(OnRefreshListener {
+            if (!et_search.text.toString().equals("")){
+                page = 1
+                getData()
+                refresh_layout.finishRefresh()
+            }
+        })
     }
 
     private fun getData(keterangan: String = "") {
-        closeKeyboard(ll_search)
-        tv_loading.visibility = View.VISIBLE
-        tv_notfound.visibility = View.GONE
-        var url = ""
-        url = "/search/users?q=${et_search.text}&page=${page}&per_page=20"
-
+        startLoading()
+        var url = "/search/users?q=${et_search.text}&page=${page}&per_page=50"
         val call: Call<DataResponse<List<Item>>> = baseApi.getUsers(url)
         call.enqueue(object : Callback<DataResponse<List<Item>>> {
             override fun onResponse(call: Call<DataResponse<List<Item>>>?, response: Response<DataResponse<List<Item>>>?) {
-                tv_loading.visibility = View.GONE
+                finishLoading()
                 if (response!!.isSuccessful) {
                     listUsers.clear()
                     listUsers.addAll(response.body()!!.items!!)
+                    refresh_layout.isEnabled = true
                     if (listUsers.size > 0) {
                         if (keterangan.equals("load")) {
                             for (data in listUsers) {
                                 adapter.lists.add(data)
                             }
-                            adapter.notifyDataSetChanged()
                         } else {
                             adapter.lists.clear()
                             adapter.lists.addAll(listUsers)
-                            adapter.notifyDataSetChanged()
                         }
+                        rv_list.visibility = View.VISIBLE
                     } else {
                         adapter.lists.clear()
-                        adapter.notifyDataSetChanged()
-                        tv_notfound.visibility = View.VISIBLE
-                        Toast.makeText(this@MainActivity, "User not found", Toast.LENGTH_LONG).show()
+                        tv_note.visibility = View.VISIBLE
+                        tv_note.text = "User not found."
+                        rv_list.visibility = View.GONE
+                        Toast.makeText(this@MainActivity, "User not found.", Toast.LENGTH_LONG).show()
                     }
+                    adapter.notifyDataSetChanged()
                 } else {
-                    Toast.makeText(this@MainActivity, response.message(), Toast.LENGTH_LONG).show()
+                    tv_note.visibility = View.VISIBLE
+                    var message = "Something wrong, please try again later."
+                    if (response.message().contains("limit")){
+                        message = "You reach your limit, please try again later."
+                    }
+                    tv_note.text = message
+                    rv_list.visibility = View.GONE
+                    Toast.makeText(this@MainActivity, message, Toast.LENGTH_LONG).show()
                 }
             }
 
             override fun onFailure(call: Call<DataResponse<List<Item>>>?, t: Throwable?) {
-                tv_loading.visibility = View.GONE
+                finishLoading()
+                tv_note.visibility = View.VISIBLE
+                rv_list.visibility = View.GONE
+                tv_note.text = t!!.message
                 Toast.makeText(this@MainActivity, t!!.message, Toast.LENGTH_LONG).show()
             }
         })
@@ -126,7 +144,7 @@ class MainActivity : BaseActivity() {
         rv_list.adapter = adapter
         initiateLinearLayoutRecyclerView(rv_list, object : RecyclerViewClickListener {
             override fun onClick(view: View, position: Int) {
-
+                Toast.makeText(this@MainActivity, adapter.lists[position].login, Toast.LENGTH_SHORT).show()
             }
 
             override fun onLongClick(view: View, position: Int) {
@@ -137,11 +155,9 @@ class MainActivity : BaseActivity() {
                 override fun onLoadMore() {
                     page += 1
                     if (listUsers.size > 19) {
-                        println("addOnScrollListener nextPage")
                         getData("load")
                     }
                 }
-
             }
         )
     }
@@ -149,5 +165,17 @@ class MainActivity : BaseActivity() {
     private fun closeKeyboard(ll_search: LinearLayout) {
         val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(ll_search.windowToken, 0)
+    }
+
+    override fun startLoading() {
+        super.startLoading()
+        closeKeyboard(ll_search)
+        tv_loading.visibility = View.VISIBLE
+        tv_note.visibility = View.GONE
+    }
+
+    override fun finishLoading() {
+        super.finishLoading()
+        tv_loading.visibility = View.GONE
     }
 }
